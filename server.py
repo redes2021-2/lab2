fc21e7443af8accd7b79159b26650a6408a078ef
GuilhermeBraz <gvcerveirabraz@gmail.com>
2022-07-10 15:04:08 -0300

    working server

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 import grpc
-import time
 import lab2_pb2
 import lab2_pb2_grpc
 from concurrent import futures
@@ -7,25 +6,23 @@ from concurrent import futures
 
 class OrderVectorServicer(lab2_pb2_grpc.OrderVectorServicer):
     def OrderVector(self, request, context):
-        response = lab2_pb2.Response()
-        # sort request vector
         vector = request.vector
+        # sort the vector
         vector.sort()
-        # find maior and menor values
-        response.maior = self.vector[-1]
-        response.menor = self.vector[0]
+        # get max and min
+        maior = vector[-1]
+        menor = vector[0]
 
-        print("Sending response: " + response)
-        return response
+        return lab2_pb2.Response(maior=maior, menor=menor)
 
 
 def main():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=3))
     lab2_pb2_grpc.add_OrderVectorServicer_to_server(
         OrderVectorServicer(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
-    print("Server started at port 50051")
+    print('Server running...')
     server.wait_for_termination()
 
 
